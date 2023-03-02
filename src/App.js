967987cc35c5251516fb8ee0ad615c4da22526f9
saimang12@naver.com <saimang12@naver.com>
2023-03-02 13:43:10 +0900

    asdf

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { DEFAULT, DEFAULT_CONTENTS, NAV } from "./data/default";
+import Header from "./pages/Header";
+import TopBanner from "./pages/TopBanner";
+const App = () => {
+    return (
+        <>
+            <TopBanner DEFAULT_CONTENTS={DEFAULT_CONTENTS} />
+            <Header DEFAULT={DEFAULT} NAV={NAV} />
+        </>
+    )
+
+}
+
+export default App;
---
+++ src/pages/TopBanner.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { BTN, Inner } from "./common";
+import { BsX } from "react-icons/bs";
+import { useState } from "react";
+
+const TopBannerWrapper = styled.div`
+    position: relative;
+    display: flex;
+    align-items:center;
+    background: #f9f9f9;
+    height: 100px;
+    overflow: hidden;
+    transition: height 0.5s;
+
+    &.on {
+        height: 0;
+    }
+
+    h2 {
+        font-size:20px;
+        font-weight:500;
+        margin: 0 0 5px 0;
+    }
+
+    button {
+        margin: 0 0 0 20px;
+    }
+
+    .x {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: (-100%,-50%);
+
+        margin: 0 0 0 600px;
+        font-size:30px;
+    }
+`
+const TopBanner = ({ DEFAULT_CONTENTS }) => {
+    const [on, setOn] = useState(false);
+
+    return (
+        <TopBannerWrapper className={on ? 'on' : ''}>
+            <Inner>
+                <h2>{DEFAULT_CONTENTS[0].title}</h2>
+                <p>{DEFAULT_CONTENTS[0].description}<BTN>more</BTN></p>
+            </Inner>
+            <BsX className="x" onClick={() => { setOn(true) }} />
+        </TopBannerWrapper>
+    )
+}
+
+export default TopBanner;
